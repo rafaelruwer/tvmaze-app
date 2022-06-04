aa4dd47e290d5a2fb124785b47bf7e288d8ef896
Rafael Victor Ruwer Araujo <rafaelruweraraujo@gmail.com>
2022-06-03 23:34:43 -0300

    Create project

---
+++ TVMazeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TVMazeApp
+//
+//  Created by Rafael Araujo on 03/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
